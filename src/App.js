5b62098396e47c7aceec60f2e9dee248dc961eba
mrc <marcoambrosini@protonmail.com>
2019-02-01 13:29:06 +0100

    ui setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
