b8ef079e07f00c655d8965ab9e9467a2232ec547
mrc <marcoambrosini@pm.me>
2019-03-06 15:14:00 +0100

    updated chatkit client

--- src/App.js
+++ src/App.js
@@ -1,123 +1,119 @@
-import React from 'react'
+import React from "react"
 
-import Chatkit from '@pusher/chatkit-client'
-import MessageList from './components/MessageList'
-import SendMessageForm from './components/SendMessageForm'
-import RoomList from './components/RoomList'
-import NewRoomForm from './components/NewRoomForm'
+import Chatkit from "@pusher/chatkit-client"
+import MessageList from "./components/MessageList"
+import SendMessageForm from "./components/SendMessageForm"
+import RoomList from "./components/RoomList"
+import NewRoomForm from "./components/NewRoomForm"
 
-import {
-    tokenUrl,
-    instanceLocator
-} from './config'
-
+import { tokenUrl, instanceLocator } from "./config"
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      roomId: null,
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
+    }
 
-    constructor() {
-        super()
-        this.state = {
-            roomId : null,
-            messages: [],
-            joinableRooms: [],
-            joinedRooms: []
+    this.sendMessage = this.sendMessage.bind(this)
+    this.subscribeToRoom = this.subscribeToRoom.bind(this)
+    this.getRooms = this.getRooms.bind(this)
+    this.createRoom = this.createRoom.bind(this)
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: "mrc",
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.currentUser = currentUser // this statement takes the currentUser thet comes from the connect method and passes it up to the component context, so that later we can use currentuser methods (currentuser.whatever()) in the app component. For example this.currentUser.sendMessage()
+        this.getRooms()
+      })
+      .catch(err => {
+        console.log("Error on connection", err)
+      })
+  }
+
+  getRooms() {
+    this.currentUser
+      .getJoinableRooms()
+      .then(joinableRooms => {
+        this.setState({
+          joinableRooms: joinableRooms,
+          joinedRooms: this.currentUser.rooms
+        })
+      })
+      .catch(err => console.log("error on joinable roomes: ", err))
+  }
+
+  subscribeToRoom(roomId) {
+    this.setState({ messages: [] })
+    this.currentUser
+      .subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+          onMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
         }
-
-        this.sendMessage = this.sendMessage.bind(this)
-        this.subscribeToRoom = this.subscribeToRoom.bind(this)
-        this.getRooms = this.getRooms.bind(this)
-        this.createRoom = this.createRoom.bind(this)
-    }
-
-
-    componentDidMount() {
-        const chatManager = new Chatkit.ChatManager({
-            instanceLocator,
-            userId: 'mrc',
-            tokenProvider: new Chatkit.TokenProvider({
-                url: tokenUrl
-            })
+      })
+      .then(room => {
+        this.setState({
+          roomId: room.id
         })
+        this.getRooms()
+      })
+      .catch(err => console.log("error on subscribing to room: ", err))
+  }
 
-        chatManager.connect()
-            .then(currentUser => {
-                this.currentUser = currentUser // this statement takes the currentUser thet comes from the connect method and passes it up to the component context, so that later we can use currentuser methods (currentuser.whatever()) in the app component. For example this.currentUser.sendMessage()
-                this.getRooms()
+  sendMessage(text) {
+    this.currentUser.sendMessage({
+      text: text,
+      roomId: this.state.roomId
+    })
+  }
 
-            })
-            .catch(err => {
-                console.log('Error on connection', err)
-            })
-    }
+  createRoom(name) {
+    this.currentUser
+      .createRoom({
+        name
+      })
+      .then(room => this.subscribeToRoom(room.id))
+      .catch(err => console.log("error while creating room ", err))
+  }
 
-    getRooms() {
-        this.currentUser.getJoinableRooms()
-            .then(joinableRooms => {
-                this.setState({
-                    joinableRooms: joinableRooms,
-                    joinedRooms: this.currentUser.rooms
-                })
-            })
-            .catch(err => console.log('error on joinable roomes: ', err))
-    }
-
-    subscribeToRoom(roomId) {
-        this.setState({ messages:[]})
-        this.currentUser.subscribeToRoom({
-            roomId: roomId,
-            hooks: {
-                onMessage: message => {
-                    this.setState({
-                        messages: [...this.state.messages, message]
-                    })
-                }
-            }
-        })
-        .then(room => {
-            this.setState ({
-                roomId : room.id
-            })
-            this.getRooms()
-        })
-        .catch(err => console.log("error on subscribing to room: ", err))
-    }
-
-    sendMessage(text) {
-        this.currentUser.sendMessage({
-            text: text, 
-            roomId: this.state.roomId
-        })
-    }
-
-    createRoom(name) {
-        this.currentUser.createRoom({
-            name
-        })
-        .then(room => this.subscribeToRoom(room.id))
-        .catch(err => console.log("error while creating room ", err))
-    }
-
-
-    render() {
-
-        return ( 
-            <div className="app">
-            <RoomList
-                roomId={this.state.roomId}
-                subscribeToRoom={this.subscribeToRoom}
-                rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} />
-            <MessageList 
-                messages={this.state.messages}
-                roomId={this.state.roomId}  
-            />
-            <SendMessageForm 
-                sendMessage={this.sendMessage} 
-                disabled={!this.state.roomId}      
-            />
-            <NewRoomForm createRoom={this.createRoom}/>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <RoomList
+          roomId={this.state.roomId}
+          subscribeToRoom={this.subscribeToRoom}
+          rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}
+        />
+        <MessageList
+          messages={this.state.messages}
+          roomId={this.state.roomId}
+        />
+        <SendMessageForm
+          sendMessage={this.sendMessage}
+          disabled={!this.state.roomId}
+        />
+        <NewRoomForm createRoom={this.createRoom} />
+      </div>
+    )
+  }
 }
 
 export default App
